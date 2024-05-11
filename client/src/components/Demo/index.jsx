5e04761f13861c32524ddd48616101fc788309f3
Toma Milkov <37478215+TomaM16@users.noreply.github.com>
2024-05-11 14:14:03 +0300

    Removed unnecessary imports

--- client/src/components/Demo/index.jsx
+++ client/src/components/Demo/index.jsx
@@ -1,9 +1,6 @@
-import { useEffect, useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 import Title from "./Title";
 import Cta from "./Cta";
-import Contract from "./Contract";
-import ContractBtns from "./ContractBtns";
 import Desc from "./Desc";
 import NoticeNoArtifact from "./NoticeNoArtifact";
 import NoticeWrongNetwork from "./NoticeWrongNetwork";
